bce554fb660106821640e6a7839047c9d3054939
Tony Rumph <tonyrumph@gmail.com>
2020-04-11 11:29:08 -0500

    Initial Commit

---
+++ AuthServer.Infrastructure/Constants/Roles.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace AuthServer.Infrastructure.Constants
+{
+    public static class Roles
+    {
+        public const string Consumer = "consumer";
+        public const string Elder = "elder";
+        public const string Servant = "servant";
+        public const string Publisher = "publisher";
+    }
+}
